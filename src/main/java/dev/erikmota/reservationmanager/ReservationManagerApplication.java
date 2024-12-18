e6f8c277f0f74524f8cd93e7a8e7a8071cafb034
Erik Mota <seriikmota.dev@gmail.com>
2024-12-17 21:13:44 -0300

    Initial commit

---
+++ src/main/java/dev/erikmota/reservationmanager/ReservationManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.erikmota.reservationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationManagerApplication.class, args);
+	}
+
+}
