2b706a65965796bc5aa6725a99e722b46ef5d0e7
Erik Mota <seriikmota.dev@gmail.com>
2025-02-11 11:27:01 -0300

    config: alter default url swagger

--- src/main/java/dev/erikmota/reservationmanager/ReservationManagerApplication.java
+++ src/main/java/dev/erikmota/reservationmanager/ReservationManagerApplication.java
@@ -1,10 +1,17 @@
 package dev.erikmota.reservationmanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 public class ReservationManagerApplication {
 
 	public static void main(String[] args) {
